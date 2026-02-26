cb59c122f625f029fa8f3abae766e65046af170d
Adam Rackis <adamrackis@hotmail.com>
2026-02-25 22:28:03 -0600

    Zustand store utility

---
+++ src/data/drizzle-zustand-store.ts
@@ -0,0 +1,30 @@
+import { create, type StoreApi, type UseBoundStore } from "zustand";
+
+type AnyPgTable = { $inferInsert: object };
+
+type DrizzleTableType<T extends AnyPgTable> = T extends {
+  $inferInsert: infer U;
+}
+  ? U
+  : never;
+
+type UpdatableObject<T> = T & {
+  performUpdate: (payload: Partial<T>) => void;
+};
+
+type DrizzleZustandStore<T> = UseBoundStore<StoreApi<UpdatableObject<T>>>;
+
+export const createDrizzleObjectStore = <T extends AnyPgTable>(
+  table: T,
+): DrizzleZustandStore<DrizzleTableType<T>> => {
+  return create<UpdatableObject<DrizzleTableType<T>>>(set => {
+    return {
+      ...(table.$inferInsert as DrizzleTableType<T>),
+      performUpdate: payload => {
+        set(state => {
+          return { ...state, ...payload };
+        });
+      },
+    };
+  });
+};
