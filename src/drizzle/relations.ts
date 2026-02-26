6aa492e899b4b7df5a0293ae9c1e82db610e747f
Adam Rackis <adamrackis@hotmail.com>
2026-02-25 21:33:55 -0600

    update Drizzle

--- src/drizzle/relations.ts
+++ src/drizzle/relations.ts
@@ -1,29 +1,53 @@
 import { relations } from "drizzle-orm/relations";
-import { workout, workoutSegment, workoutSegmentExercise, exercises } from "./schema";
+import { workoutTemplate, workoutTemplateSegment, workoutTemplateSegmentExercise, exercises, workout, workoutSegment, workoutSegmentExercise } from "./schema";
 
-export const workoutSegmentRelations = relations(workoutSegment, ({ one, many }) => ({
-  workout: one(workout, {
-    fields: [workoutSegment.workoutId],
-    references: [workout.id],
-  }),
-  workoutSegmentExercises: many(workoutSegmentExercise),
+export const workoutTemplateSegmentRelations = relations(workoutTemplateSegment, ({one, many}) => ({
+	workoutTemplate: one(workoutTemplate, {
+		fields: [workoutTemplateSegment.workoutTemplateId],
+		references: [workoutTemplate.id]
+	}),
+	workoutTemplateSegmentExercises: many(workoutTemplateSegmentExercise),
 }));
 
-export const workoutRelations = relations(workout, ({ many }) => ({
-  workoutSegments: many(workoutSegment),
+export const workoutTemplateRelations = relations(workoutTemplate, ({many}) => ({
+	workoutTemplateSegments: many(workoutTemplateSegment),
 }));
 
-export const workoutSegmentExerciseRelations = relations(workoutSegmentExercise, ({ one }) => ({
-  workoutSegment: one(workoutSegment, {
-    fields: [workoutSegmentExercise.workoutSegmentId],
-    references: [workoutSegment.id],
-  }),
-  exercise: one(exercises, {
-    fields: [workoutSegmentExercise.exerciseId],
-    references: [exercises.id],
-  }),
+export const workoutTemplateSegmentExerciseRelations = relations(workoutTemplateSegmentExercise, ({one}) => ({
+	workoutTemplateSegment: one(workoutTemplateSegment, {
+		fields: [workoutTemplateSegmentExercise.workoutTemplateSegmentId],
+		references: [workoutTemplateSegment.id]
+	}),
+	exercise: one(exercises, {
+		fields: [workoutTemplateSegmentExercise.exerciseId],
+		references: [exercises.id]
+	}),
 }));
 
-export const exercisesRelations = relations(exercises, ({ many }) => ({
-  workoutSegmentExercises: many(workoutSegmentExercise),
+export const exercisesRelations = relations(exercises, ({many}) => ({
+	workoutTemplateSegmentExercises: many(workoutTemplateSegmentExercise),
+	workoutSegmentExercises: many(workoutSegmentExercise),
+}));
+
+export const workoutSegmentRelations = relations(workoutSegment, ({one, many}) => ({
+	workout: one(workout, {
+		fields: [workoutSegment.workoutId],
+		references: [workout.id]
+	}),
+	workoutSegmentExercises: many(workoutSegmentExercise),
+}));
+
+export const workoutRelations = relations(workout, ({many}) => ({
+	workoutSegments: many(workoutSegment),
+}));
+
+export const workoutSegmentExerciseRelations = relations(workoutSegmentExercise, ({one}) => ({
+	workoutSegment: one(workoutSegment, {
+		fields: [workoutSegmentExercise.workoutSegmentId],
+		references: [workoutSegment.id]
+	}),
+	exercise: one(exercises, {
+		fields: [workoutSegmentExercise.exerciseId],
+		references: [exercises.id]
+	}),
 }));
--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,104 +1,121 @@
-import { pgTable, unique, integer, varchar, timestamp, text, index, foreignKey, check, boolean, pgEnum } from "drizzle-orm/pg-core";
-import { sql } from "drizzle-orm";
+import { pgTable, unique, integer, varchar, timestamp, text, index, foreignKey, check, boolean, date, pgEnum } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
 
-export const muscleGroup = pgEnum("muscle_group", ["chest", "shoulders", "biceps", "triceps", "quadriceps", "hamstrings", "calves", "lats", "back"]);
+export const muscleGroup = pgEnum("muscle_group", ['chest', 'shoulders', 'biceps', 'triceps', 'quadriceps', 'hamstrings', 'calves', 'lats', 'back'])
 
-export const users = pgTable(
-  "users",
-  {
-    id: integer()
-      .primaryKey()
-      .generatedAlwaysAsIdentity({ name: "users_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
-    username: varchar({ length: 250 }).notNull(),
-    password: varchar({ length: 250 }).notNull(),
-    displayName: varchar("display_name", { length: 250 }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true, mode: "string" }).defaultNow().notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true, mode: "string" }).defaultNow().notNull(),
-  },
-  (table) => [unique("users_username_key").on(table.username)]
-);
 
-export const workout = pgTable("workout", {
-  id: integer()
-    .primaryKey()
-    .generatedAlwaysAsIdentity({ name: "workout_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
-  name: varchar({ length: 50 }),
-  description: text(),
+export const users = pgTable("users", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "users_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	username: varchar({ length: 250 }).notNull(),
+	password: varchar({ length: 250 }).notNull(),
+	displayName: varchar("display_name", { length: 250 }).notNull(),
+	createdAt: timestamp("created_at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+	updatedAt: timestamp("updated_at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+}, (table) => [
+	unique("users_username_key").on(table.username),
+]);
+
+export const workoutTemplate = pgTable("workout_template", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "workout_template_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	name: varchar({ length: 50 }).notNull(),
+	description: text().default(').notNull(),
 });
 
-export const workoutSegment = pgTable(
-  "workout_segment",
-  {
-    id: integer()
-      .primaryKey()
-      .generatedAlwaysAsIdentity({ name: "workout_segment_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
-    workoutId: integer("workout_id").notNull(),
-    segmentOrder: integer("segment_order").notNull(),
-    sets: integer().notNull(),
-  },
-  (table) => [
-    index("idx_workout_segment_workout_id_segment_order").using(
-      "btree",
-      table.workoutId.asc().nullsLast().op("int4_ops"),
-      table.segmentOrder.asc().nullsLast().op("int4_ops")
-    ),
-    foreignKey({
-      columns: [table.workoutId],
-      foreignColumns: [workout.id],
-      name: "workout_segment_workout_id_fkey",
-    }).onDelete("cascade"),
-    check("workout_segment_segment_order_check", sql`segment_order > 0`),
-    check("workout_segment_sets_check", sql`sets > 0`),
-  ]
-);
+export const workoutTemplateSegment = pgTable("workout_template_segment", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "workout_template_segment_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	workoutTemplateId: integer("workout_template_id").notNull(),
+	segmentOrder: integer("segment_order").notNull(),
+	sets: integer().notNull(),
+}, (table) => [
+	index("idx_workout_template_segment_template_id_segment_order").using("btree", table.workoutTemplateId.asc().nullsLast().op("int4_ops"), table.segmentOrder.asc().nullsLast().op("int4_ops")),
+	foreignKey({
+			columns: [table.workoutTemplateId],
+			foreignColumns: [workoutTemplate.id],
+			name: "workout_template_segment_workout_template_id_fkey"
+		}).onDelete("cascade"),
+	check("workout_template_segment_segment_order_check", sql`segment_order > 0`),
+	check("workout_template_segment_sets_check", sql`sets > 0`),
+]);
 
-export const workoutSegmentExercise = pgTable(
-  "workout_segment_exercise",
-  {
-    id: integer().primaryKey().generatedAlwaysAsIdentity({
-      name: "workout_segment_exercise_id_seq",
-      startWith: 1,
-      increment: 1,
-      minValue: 1,
-      maxValue: 2147483647,
-      cache: 1,
-    }),
-    workoutSegmentId: integer("workout_segment_id").notNull(),
-    exerciseOrder: integer("exercise_order").notNull(),
-    exerciseId: integer("exercise_id").notNull(),
-    reps: integer().notNull(),
-  },
-  (table) => [
-    index("idx_workout_segment_exercise_segment_id_exercise_order").using(
-      "btree",
-      table.workoutSegmentId.asc().nullsLast().op("int4_ops"),
-      table.exerciseOrder.asc().nullsLast().op("int4_ops")
-    ),
-    foreignKey({
-      columns: [table.workoutSegmentId],
-      foreignColumns: [workoutSegment.id],
-      name: "workout_segment_exercise_workout_segment_id_fkey",
-    }).onDelete("cascade"),
-    foreignKey({
-      columns: [table.exerciseId],
-      foreignColumns: [exercises.id],
-      name: "workout_segment_exercise_exercise_id_fkey",
-    }),
-    check("workout_segment_exercise_exercise_order_check", sql`exercise_order > 0`),
-    check("workout_segment_exercise_reps_check", sql`reps > 0`),
-  ]
-);
+export const workoutTemplateSegmentExercise = pgTable("workout_template_segment_exercise", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "workout_template_segment_exercise_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	workoutTemplateSegmentId: integer("workout_template_segment_id").notNull(),
+	exerciseOrder: integer("exercise_order").notNull(),
+	exerciseId: integer("exercise_id").notNull(),
+	reps: integer(),
+	repsToFailure: boolean("reps_to_failure").notNull(),
+}, (table) => [
+	index("idx_workout_template_segment_exercise_segment_id_exercise_order").using("btree", table.workoutTemplateSegmentId.asc().nullsLast().op("int4_ops"), table.exerciseOrder.asc().nullsLast().op("int4_ops")),
+	foreignKey({
+			columns: [table.workoutTemplateSegmentId],
+			foreignColumns: [workoutTemplateSegment.id],
+			name: "workout_template_segment_exerc_workout_template_segment_id_fkey"
+		}).onDelete("cascade"),
+	foreignKey({
+			columns: [table.exerciseId],
+			foreignColumns: [exercises.id],
+			name: "workout_template_segment_exercise_exercise_id_fkey"
+		}),
+	check("workout_template_segment_exercise_exercise_order_check", sql`exercise_order > 0`),
+	check("workout_template_segment_exercise_reps_check", sql`reps > 0`),
+	check("workout_template_segment_exercise_check", sql`(reps_to_failure = true) OR (reps IS NOT NULL)`),
+]);
 
-export const exercises = pgTable(
-  "exercises",
-  {
-    id: integer()
-      .primaryKey()
-      .generatedAlwaysAsIdentity({ name: "exercises_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
-    name: varchar({ length: 150 }),
-    description: text(),
-    muscleGroups: muscleGroup("muscle_groups").array(),
-    isCompound: boolean("is_compound"),
-  },
-  (table) => [index("idx_exercises_muscle_groups_gin").using("gin", table.muscleGroups.asc().nullsLast().op("array_ops"))]
-);
+export const exercises = pgTable("exercises", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "exercises_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	name: varchar({ length: 150 }),
+	description: text(),
+	muscleGroups: muscleGroup("muscle_groups").array(),
+	isCompound: boolean("is_compound"),
+}, (table) => [
+	index("idx_exercises_muscle_groups_gin").using("gin", table.muscleGroups.asc().nullsLast().op("array_ops")),
+]);
+
+export const workout = pgTable("workout", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "workout_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	name: varchar({ length: 50 }).notNull(),
+	description: text().default(').notNull(),
+	workoutDate: date("workout_date").notNull(),
+}, (table) => [
+	index("idx_workout_workout_date").using("btree", table.workoutDate.asc().nullsLast().op("date_ops")),
+]);
+
+export const workoutSegment = pgTable("workout_segment", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "workout_segment_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	workoutId: integer("workout_id").notNull(),
+	segmentOrder: integer("segment_order").notNull(),
+	sets: integer().notNull(),
+}, (table) => [
+	index("idx_workout_segment_workout_id_segment_order").using("btree", table.workoutId.asc().nullsLast().op("int4_ops"), table.segmentOrder.asc().nullsLast().op("int4_ops")),
+	foreignKey({
+			columns: [table.workoutId],
+			foreignColumns: [workout.id],
+			name: "workout_segment_workout_id_fkey"
+		}).onDelete("cascade"),
+	check("workout_segment_segment_order_check", sql`segment_order > 0`),
+	check("workout_segment_sets_check", sql`sets > 0`),
+]);
+
+export const workoutSegmentExercise = pgTable("workout_segment_exercise", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "workout_segment_exercise_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
+	workoutSegmentId: integer("workout_segment_id").notNull(),
+	exerciseOrder: integer("exercise_order").notNull(),
+	exerciseId: integer("exercise_id").notNull(),
+	reps: integer(),
+	repsToFailure: boolean("reps_to_failure").notNull(),
+}, (table) => [
+	index("idx_workout_segment_exercise_segment_id_exercise_order").using("btree", table.workoutSegmentId.asc().nullsLast().op("int4_ops"), table.exerciseOrder.asc().nullsLast().op("int4_ops")),
+	foreignKey({
+			columns: [table.workoutSegmentId],
+			foreignColumns: [workoutSegment.id],
+			name: "workout_segment_exercise_workout_segment_id_fkey"
+		}).onDelete("cascade"),
+	foreignKey({
+			columns: [table.exerciseId],
+			foreignColumns: [exercises.id],
+			name: "workout_segment_exercise_exercise_id_fkey"
+		}),
+	check("workout_segment_exercise_exercise_order_check", sql`exercise_order > 0`),
+	check("workout_segment_exercise_reps_check", sql`reps > 0`),
+	check("workout_segment_exercise_check", sql`(reps_to_failure = true) OR (reps IS NOT NULL)`),
+]);
