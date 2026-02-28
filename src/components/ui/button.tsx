952e35c57b46c6fd605012bf48b3501b82c8ed03
Adam Rackis <adamrackis@hotmail.com>
2026-02-27 19:39:50 -0600

    tweak secondary button outline

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "border border-border/70 bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border border-border bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
