6f3fce514c2e4bfda946891a873fb6ef7d8d6789
Adam Rackis <adamrackis@hotmail.com>
2026-03-03 08:35:30 -0600

    Tweak admin route nav

--- src/routes/app/admin/route.tsx
+++ src/routes/app/admin/route.tsx
@@ -31,6 +31,7 @@ function RouteComponent() {
         <Link
           to="/app/admin/workout-templates"
           className={navLinkClassName}
+          activeOptions={{ exact: true }}
           activeProps={activeNavLinkProps}
           inactiveProps={inactiveNavLinkProps}
         >
