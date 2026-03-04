30b12da12e8fc8af291190f33c63724cf25ce60f
Adam Rackis <adamrackis@hotmail.com>
2026-03-04 02:04:31 -0600

    Attempt to integrate query ssr

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,22 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 import { getContext } from "./integrations/tanstack-query/root-provider";
 
 export function getRouter() {
+  const context = getContext();
   const router = createTanStackRouter({
     routeTree,
 
-    context: getContext(),
+    context,
 
     scrollRestoration: true,
     defaultPreload: "intent",
     defaultPreloadStaleTime: 0,
   });
+
+  setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient });
 
   return router;
 }
