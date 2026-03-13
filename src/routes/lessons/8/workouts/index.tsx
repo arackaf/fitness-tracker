bf51b15383958fed38a16dfd43913eeb802d62b3
Adam Rackis <adamrackis@hotmail.com>
2026-03-13 17:49:00 -0400

    Work on lesson 8

--- src/routes/lessons/8/workouts/index.tsx
+++ src/routes/lessons/8/workouts/index.tsx
@@ -1,10 +1,15 @@
-import { useMemo, useState } from "react";
+import { forwardRef, useMemo, useRef, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { ExerciseSelector } from "@/components/ExerciseSelector";
+import { Input } from "@/components/ui/input";
 import { getExercisesServerFn } from "@/server-functions/exercises";
 import { getInClassWorkoutHistory } from "@/server-functions/in-class/workouts-simple";
+
+type ArrayOf<T> = T extends Array<infer U> ? U : never;
+type Workout = ArrayOf<Awaited<ReturnType<typeof getInClassWorkoutHistory>>>;
+type Exercise = ArrayOf<Awaited<ReturnType<typeof getExercisesServerFn>>>;
 
 export const Route = createFileRoute("/lessons/8/workouts/")({
   component: RouteComponent,
@@ -16,6 +21,7 @@ function RouteComponent() {
   const [selectedExerciseId, setSelectedExerciseId] = useState<number | null>(
     null,
   );
+  const selectedExerciseNameInputRef = useRef<HTMLInputElement>(null);
 
   const { data: exercises, isLoading: isExercisesPending } = useQuery({
     queryKey: ["exercises"],
@@ -29,6 +35,9 @@ function RouteComponent() {
   const exerciseLookup = useMemo(() => {
     return new Map((exercises ?? []).map(exercise => [exercise.id, exercise]));
   }, [exercises]);
+  const selectedExercise = exercises?.find(
+    exercise => exercise.id === selectedExerciseId,
+  );
 
   return (
     <div className="flex flex-col gap-4">
@@ -44,27 +53,55 @@ function RouteComponent() {
           }}
         />
       )}
+      {selectedExercise ? (
+        <EditExercise
+          key={selectedExercise.id}
+          ref={selectedExerciseNameInputRef}
+          exerciseName={selectedExercise.name}
+        />
+      ) : null}
+      <div className="border-t" />
       {isWorkoutsPending || !workouts ? (
         <span>Loading workouts...</span>
       ) : (
         workouts.map(workout => (
-          <div key={workout.id}>
-            <span className="flex gap-2">
-              <span>{workout.name}</span>
-              <span>Exercises:</span>
-              <span>
-                (
-                {workout.exercises
-                  .map(
-                    exercise => exerciseLookup.get(exercise)?.name ?? exercise,
-                  )
-                  .join(", ")}
-                )
-              </span>
-            </span>
-          </div>
+          <WorkoutRow
+            key={workout.id}
+            workout={workout}
+            exerciseLookup={exerciseLookup}
+          />
         ))
       )}
+    </div>
+  );
+}
+
+const EditExercise = forwardRef<HTMLInputElement, { exerciseName: string }>(
+  function SelectedExerciseNameInput({ exerciseName }, ref) {
+    return <Input ref={ref} defaultValue={exerciseName} />;
+  },
+);
+
+function WorkoutRow({
+  workout,
+  exerciseLookup,
+}: {
+  workout: Workout;
+  exerciseLookup: Map<number, Exercise>;
+}) {
+  return (
+    <div>
+      <span className="flex gap-2">
+        <span>{workout.name}</span>
+        <span>Exercises:</span>
+        <span>
+          (
+          {workout.exercises
+            .map(exercise => exerciseLookup.get(exercise)?.name ?? exercise)
+            .join(", ")}
+          )
+        </span>
+      </span>
     </div>
   );
 }
