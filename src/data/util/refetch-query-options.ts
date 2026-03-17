68e62987beea479a6353394d338d67591977c4af
Adam Rackis <adamrackis@hotmail.com>
2026-03-17 17:57:10 -0400

    refetch query options work

--- src/data/util/refetch-query-options.ts
+++ src/data/util/refetch-query-options.ts
@@ -1,4 +1,14 @@
+import { type RequiredFetcher } from "@tanstack/react-start";
 import { queryOptions, type QueryKey } from "@tanstack/react-query";
+
+export type AnyServerFn = RequiredFetcher<any, any, any>;
+export type RefetchPayload<
+  TFn extends RequiredFetcher<any, any, any> = AnyServerFn,
+> = {
+  key: any;
+  fn: TFn;
+  arg: Parameters<TFn>[0]["data"];
+};
 
 export function refetchedQueryOptions<
   T extends (arg: { data: any }) => Promise<any>,
@@ -14,7 +24,7 @@ export function refetchedQueryOptions<
     },
     meta: {
       __revalidate: {
-        serverFn,
+        fn: serverFn,
         arg,
       },
     },
