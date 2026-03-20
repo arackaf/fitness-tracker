b2014c4add05288ba35dce6ca9923e7b450b3d0f
Adam Rackis <adamrackis@hotmail.com>
2026-03-20 14:34:46 -0700

    Add default weight unit

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,4 +1,5 @@
-import type { DistanceUnit, DurationUnit } from "./types";
+import type { DistanceUnit, DurationUnit, ExerciseWeightUnit } from "./types";
 
 export const defaultDurationUnit: DurationUnit = "minutes";
 export const defaultDistanceUnit: DistanceUnit = "miles";
+export const defaultExerciseWeightUnit: ExerciseWeightUnit = "lbs";
--- src/data/types.ts
+++ src/data/types.ts
@@ -2,7 +2,12 @@ import type { PgAsyncDatabase } from "drizzle-orm/pg-core";
 import type { NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
 import type { PgliteQueryResultHKT } from "drizzle-orm/pglite";
 import type { DbSchema } from "./drizzle-schema";
-import { distanceUnit, durationUnit, type muscleGroup } from "@/drizzle/schema";
+import {
+  distanceUnit,
+  durationUnit,
+  exerciseWeightUnit,
+  type muscleGroup,
+} from "@/drizzle/schema";
 
 export type DbType = PgAsyncDatabase<
   NodePgQueryResultHKT | PgliteQueryResultHKT,
@@ -13,3 +18,4 @@ export type MuscleGroup = typeof muscleGroup.$inferSelect;
 
 export type DurationUnit = (typeof durationUnit.enumValues)[number];
 export type DistanceUnit = (typeof distanceUnit.enumValues)[number];
+export type ExerciseWeightUnit = (typeof exerciseWeightUnit.enumValues)[number];
