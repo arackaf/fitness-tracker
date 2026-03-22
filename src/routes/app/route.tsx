b6a1532775293834078ae5e29d69c9b4c9002a27
Adam Rackis <adamrackis@hotmail.com>
2026-03-22 13:56:36 -0700

    tweak responsiveness

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -160,7 +160,7 @@ function RouteComponent() {
           </Sheet>
         </div>
       </header>
-      <div className="mx-auto w-full max-w-4xl px-6 py-10 md:px-8">
+      <div className="mx-auto w-full max-w-4xl px-6 py-6 md:py-10 md:px-8">
         <Outlet />
       </div>
     </main>
