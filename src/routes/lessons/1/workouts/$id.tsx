aa31cfc0b05841568ab5fb6b7b36355206730259
Adam Rackis <adamrackis@hotmail.com>
2026-03-24 17:25:31 -0500

    cleanup

--- src/routes/lessons/1/workouts/$id.tsx
+++ src/routes/lessons/1/workouts/$id.tsx
@@ -1,17 +1,4 @@
-import { useMemo } from "react";
 import { createFileRoute, Link } from "@tanstack/react-router";
-
-type Workout = {
-  id: number;
-  name: string;
-  date: string;
-  exercises: number[];
-};
-
-type Exercise = {
-  id: number;
-  name: string;
-};
 
 export const Route = createFileRoute("/lessons/1/workouts/$id")({
   component: RouteComponent,
