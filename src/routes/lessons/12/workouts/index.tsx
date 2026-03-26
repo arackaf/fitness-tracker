70777ed3f562c938fb4dc3cc513636e0d24520ed
Adam Rackis <adamrackis@hotmail.com>
2026-03-26 16:10:13 -0500

    set up server function

--- src/routes/lessons/12/workouts/index.tsx
+++ src/routes/lessons/12/workouts/index.tsx
@@ -1,9 +1,33 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn, useServerFn } from "@tanstack/react-start";
 
 export const Route = createFileRoute("/lessons/12/workouts/")({
   component: RouteComponent,
 });
 
+const serverFn = createServerFn({ method: "GET" }).handler(async () => {
+  console.log(
+    "\n",
+    "================================",
+    "I'm in a server function!!!",
+    "================================",
+    "\n",
+  );
+  return {
+    message: "Hello, world!",
+  };
+});
+
 function RouteComponent() {
-  return <div>Nothing to see here</div>;
+  const call = useServerFn(serverFn);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <span>Click this button!</span>
+      <Button onClick={() => call()} variant="secondary">
+        Click me
+      </Button>
+    </div>
+  );
 }
