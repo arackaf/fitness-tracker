e07fa9e11a220aaebd47f48b04707dfd3043d0c2
Adam Rackis <adamrackis@hotmail.com>
2026-05-04 21:22:21 -0500

    test utility

--- src/components/display-workout/DisplayReps.test.ts
+++ src/components/display-workout/DisplayReps.test.ts
@@ -1,4 +1,8 @@
-import type { Exercise, SegmentWithExercises, WorkoutSegmentExerciseMeasurementState } from "@/data/workouts/workout-state";
+import type {
+  Exercise,
+  SegmentWithExercises,
+  WorkoutSegmentExerciseMeasurementState,
+} from "@/data/workouts/workout-state";
 import { describe, expect, test } from "vitest";
 import { getDisplayReps } from "./DisplayReps";
 
@@ -7,6 +11,28 @@ const pushup: Exercise = {
   executionType: "repetition",
   exerciseOrder: 0,
   measurements: [],
+};
+
+type SegmentInput = [
+  exercise: Exercise,
+  measurements: Omit<Exercise["measurements"][number], "setOrder">[],
+];
+const constructSegment = (
+  input: SegmentInput[],
+  sets: number | null = null,
+): SegmentWithExercises => {
+  const setCount = sets ?? input[0][1].length;
+
+  return {
+    segmentOrder: 0,
+    exercises: input.map(([exercise, measurements]) => {
+      return {
+        ...exercise,
+        measurements: measurements.map(m => ({ ...m, setOrder: 0 })),
+      };
+    }),
+    sets: setCount,
+  };
 };
 
 describe("Reps no weight", function () {
@@ -22,6 +48,10 @@ describe("Reps no weight", function () {
       exercises: [Object.assign({}, pushup, { measurements })],
       sets: 4,
     };
-    expect(getDisplayReps(segment)).toBe("20, 20, 20, 20");
+    expect(
+      getDisplayReps(
+        constructSegment([[pushup, [{ reps: 20 }, { reps: 20 }, { reps: 20 }, { reps: 20 }]]]),
+      ),
+    ).toBe("20, 20, 20, 20");
   });
 });
