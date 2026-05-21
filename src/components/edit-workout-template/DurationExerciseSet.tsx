cd9e56b6fb461dd032abede532e8ea6ddfe82bd8
Adam Rackis <adamrackis@hotmail.com>
2026-05-21 15:23:03 -0500

    duration template set fixed

--- src/components/edit-workout-template/DurationExerciseSet.tsx
+++ src/components/edit-workout-template/DurationExerciseSet.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import type { WorkoutTemplateForm } from "@/lib/workout-template-form";
 import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
 
 type DurationExerciseSetProps = {
   form: WorkoutTemplateForm;
@@ -15,77 +16,71 @@ export const DurationExerciseSet: FC<DurationExerciseSetProps> = ({ form, segmen
   return (
     <div className="flex gap-2 min-h-7">
       <div className="flex text-sm items-start gap-2">
-        <div className="h-7 flex items-center">
-          <span className="font-medium">Duration</span>
-        </div>
-        <div className="flex flex-wrap items-start gap-2">
-          <form.Field
-            mode="array"
-            name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements`}
-            children={field => {
-              return field.state.value?.map((_, measurementIndex) => {
-                const setNumber = measurementIndex + 1;
+        <form.Field
+          mode="array"
+          name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements`}
+          children={field => {
+            return field.state.value?.map((_, measurementIndex) => {
+              const setNumber = measurementIndex + 1;
 
-                return (
-                  <div className="flex gap-1">
-                    <span className="flex items-center h-7">{setNumber}:</span>
-                    <div key={`duration-${setNumber}`} className="flex flex-col gap-1.5">
-                      <form.Field
-                        name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].duration`}
-                        children={durationField => (
-                          <label className="h-7 inline-flex items-start gap-1 text-xs text-muted-foreground">
-                            <Input
-                              maxLength={50}
-                              value={durationField.state.value ?? ""}
-                              onChange={event => {
-                                const value = event.target.value;
-                                durationField.handleChange(value);
-                              }}
-                              className={cn(
-                                "h-7 w-24 px-2 py-1",
-                                !durationField.state.meta.isValid ? "border-red-500" : "",
-                              )}
-                            />
-                          </label>
-                        )}
-                      />
-                      {measurementIndex === 0 ? (
-                        <Button
-                          type="button"
-                          variant="secondary"
-                          size="sm"
-                          className="w-fit h-5 cursor-pointer"
-                          onClick={() => {
-                            const measurementFieldName =
-                              `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements` as const;
-                            const measurements = field.state.value;
-                            const sourceMeasurement = measurements[measurementIndex];
+              return (
+                <div className="flex gap-1 items-center">
+                  <span className="flex items-center h-7">{setNumber}:</span>
 
-                            form.setFieldValue(
-                              measurementFieldName,
-                              measurements.map((measurement, targetMeasurementIndex) => {
-                                if (targetMeasurementIndex === measurementIndex) {
-                                  return measurement;
-                                }
-
-                                return {
-                                  ...measurement,
-                                  duration: sourceMeasurement.duration,
-                                };
-                              }),
-                            );
+                  <form.Field
+                    name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].duration`}
+                    children={durationField => (
+                      <label className="h-7 inline-flex items-start gap-1 text-xs text-muted-foreground">
+                        <Input
+                          maxLength={50}
+                          value={durationField.state.value ?? ""}
+                          onChange={event => {
+                            const value = event.target.value;
+                            durationField.handleChange(value);
                           }}
-                        >
-                          Fill
-                        </Button>
-                      ) : null}
-                    </div>
-                  </div>
-                );
-              });
-            }}
-          />
-        </div>
+                          className={cn(
+                            "h-7 w-24 px-2 py-1",
+                            !durationField.state.meta.isValid ? "border-red-500" : "",
+                          )}
+                        />
+                      </label>
+                    )}
+                  />
+                  {measurementIndex === 0 ? (
+                    <Button
+                      type="button"
+                      variant="secondary"
+                      size="sm"
+                      className="w-fit h-5 cursor-pointer px-1!"
+                      onClick={() => {
+                        const measurementFieldName =
+                          `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements` as const;
+                        const measurements = field.state.value;
+                        const sourceMeasurement = measurements[measurementIndex];
+
+                        form.setFieldValue(
+                          measurementFieldName,
+                          measurements.map((measurement, targetMeasurementIndex) => {
+                            if (targetMeasurementIndex === measurementIndex) {
+                              return measurement;
+                            }
+
+                            return {
+                              ...measurement,
+                              duration: sourceMeasurement.duration,
+                            };
+                          }),
+                        );
+                      }}
+                    >
+                      <ChevronRight />
+                    </Button>
+                  ) : null}
+                </div>
+              );
+            });
+          }}
+        />
       </div>
     </div>
   );
