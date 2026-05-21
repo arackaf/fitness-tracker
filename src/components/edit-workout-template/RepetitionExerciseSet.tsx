fb2363056b152bafba8238094c0e3947028e4975
Adam Rackis <adamrackis@hotmail.com>
2026-05-21 15:26:59 -0500

    repetition set layout fix

--- src/components/edit-workout-template/RepetitionExerciseSet.tsx
+++ src/components/edit-workout-template/RepetitionExerciseSet.tsx
@@ -5,6 +5,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import type { WorkoutTemplateForm } from "@/lib/workout-template-form";
 import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
 
 type RepetitionExerciseSetProps = {
   form: WorkoutTemplateForm;
@@ -22,119 +23,107 @@ export const RepetitionExerciseSet: FC<RepetitionExerciseSetProps> = ({
   return (
     <div className="flex gap-2 min-h-7">
       <div className="flex text-sm items-start gap-2">
-        <div className="h-7 flex items-center">
-          <span className="font-medium">Reps</span>
-        </div>
-        <div className="flex flex-wrap gap-2">
-          <form.Field
-            mode="array"
-            name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements`}
-            children={field => {
-              return field.state.value?.map((_, measurementIndex) => {
-                const setNumber = measurementIndex + 1;
-                return (
-                  <div
-                    key={`segment-${segmentIndex}-exercise-${exerciseIndex}-reps-${setNumber}`}
-                    className="flex gap-1"
-                  >
-                    <span className="h-7 inline-flex items-center">{setNumber}:</span>
-                    <div className="flex flex-col gap-1.5">
-                      <div className="flex flex-wrap gap-2">
-                        <form.Field
-                          name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].reps`}
-                          children={repsField => (
-                            <label
-                              key={`reps-${setNumber}`}
-                              className="h-7 inline-flex items-center gap-1 text-xs text-muted-foreground"
-                            >
-                              <Input
-                                maxLength={50}
-                                value={repsField.state.value ?? ""}
-                                onChange={event => {
-                                  const value = event.target.value;
-                                  repsField.handleChange(value);
-                                }}
-                                className={cn(
-                                  "h-7 w-16 px-2 py-1",
-                                  !repsField.state.meta.isValid ? "border-red-500" : "",
-                                )}
-                              />
-                            </label>
-                          )}
-                        />
-                        {showWeightUsed ? (
-                          <form.Field
-                            name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].weightUsed`}
-                            children={weightUsedField => (
-                              <label className="h-7 inline-flex items-center gap-1 text-xs text-muted-foreground">
-                                <Input
-                                  maxLength={50}
-                                  value={weightUsedField.state.value ?? ""}
-                                  onChange={event => {
-                                    const value = event.target.value;
-                                    weightUsedField.handleChange(value);
-                                  }}
-                                  className={cn("h-7 w-18 px-2 py-1")}
-                                />
-                              </label>
-                            )}
+        <form.Field
+          mode="array"
+          name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements`}
+          children={field => {
+            return field.state.value?.map((_, measurementIndex) => {
+              const setNumber = measurementIndex + 1;
+              return (
+                <div className="flex gap-1 items-center">
+                  <span className="h-7 inline-flex items-center">{setNumber}:</span>
+
+                  <div className="flex flex-wrap gap-2">
+                    <form.Field
+                      name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].reps`}
+                      children={repsField => (
+                        <label
+                          key={`reps-${setNumber}`}
+                          className="h-7 inline-flex items-center gap-1 text-xs text-muted-foreground"
+                        >
+                          <Input
+                            maxLength={50}
+                            value={repsField.state.value ?? ""}
+                            onChange={event => {
+                              const value = event.target.value;
+                              repsField.handleChange(value);
+                            }}
+                            className={cn("h-7 w-16 px-2 py-1", !repsField.state.meta.isValid ? "border-red-500" : "")}
                           />
-                        ) : null}
-                      </div>
+                        </label>
+                      )}
+                    />
+                    {showWeightUsed ? (
                       <form.Field
-                        name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].repsToFailure`}
-                        children={repsToFailureField => (
-                          <label className="inline-flex items-center gap-0.5 text-xs text-muted-foreground">
-                            <Checkbox
-                              checked={repsToFailureField.state.value ?? false}
-                              onCheckedChange={checked => {
-                                repsToFailureField.handleChange(checked === true);
+                        name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].weightUsed`}
+                        children={weightUsedField => (
+                          <label className="h-7 inline-flex items-center gap-1 text-xs text-muted-foreground">
+                            <Input
+                              maxLength={50}
+                              value={weightUsedField.state.value ?? ""}
+                              onChange={event => {
+                                const value = event.target.value;
+                                weightUsedField.handleChange(value);
                               }}
+                              className={cn("h-7 w-18 px-2 py-1")}
                             />
-                            To failure
                           </label>
                         )}
                       />
-                      {measurementIndex === 0 ? (
-                        <Button
-                          type="button"
-                          variant="secondary"
-                          size="sm"
-                          className="w-fit h-5 cursor-pointer"
-                          onClick={() => {
-                            const measurements = field.state.value;
-                            const sourceMeasurement = measurements[measurementIndex];
-
-                            for (let i = 1; i < measurements.length; i++) {
-                              if (sourceMeasurement.reps != "") {
-                                form.setFieldValue(
-                                  `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${i}].reps`,
-                                  sourceMeasurement.reps,
-                                );
-                              }
-                              if (sourceMeasurement.weightUsed != "") {
-                                form.setFieldValue(
-                                  `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${i}].weightUsed`,
-                                  sourceMeasurement.weightUsed,
-                                );
-                              }
-                              form.setFieldValue(
-                                `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${i}].repsToFailure`,
-                                sourceMeasurement.repsToFailure,
-                              );
-                            }
-                          }}
-                        >
-                          Fill
-                        </Button>
-                      ) : null}
-                    </div>
+                    ) : null}
                   </div>
-                );
-              });
-            }}
-          />
-        </div>
+                  <form.Field
+                    name={`segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${measurementIndex}].repsToFailure`}
+                    children={repsToFailureField => (
+                      <label className="inline-flex items-center gap-0.5 text-xs text-muted-foreground">
+                        <Checkbox
+                          checked={repsToFailureField.state.value ?? false}
+                          onCheckedChange={checked => {
+                            repsToFailureField.handleChange(checked === true);
+                          }}
+                        />
+                        To failure
+                      </label>
+                    )}
+                  />
+                  {measurementIndex === 0 ? (
+                    <Button
+                      type="button"
+                      variant="secondary"
+                      size="sm"
+                      className="w-fit h-5 cursor-pointer"
+                      onClick={() => {
+                        const measurements = field.state.value;
+                        const sourceMeasurement = measurements[measurementIndex];
+
+                        for (let i = 1; i < measurements.length; i++) {
+                          if (sourceMeasurement.reps != "") {
+                            form.setFieldValue(
+                              `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${i}].reps`,
+                              sourceMeasurement.reps,
+                            );
+                          }
+                          if (sourceMeasurement.weightUsed != "") {
+                            form.setFieldValue(
+                              `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${i}].weightUsed`,
+                              sourceMeasurement.weightUsed,
+                            );
+                          }
+                          form.setFieldValue(
+                            `segments[${segmentIndex}].exercises[${exerciseIndex}].measurements[${i}].repsToFailure`,
+                            sourceMeasurement.repsToFailure,
+                          );
+                        }
+                      }}
+                    >
+                      <ChevronRight />
+                    </Button>
+                  ) : null}
+                </div>
+              );
+            });
+          }}
+        />
       </div>
     </div>
   );
