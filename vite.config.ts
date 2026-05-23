4a1eed8774ae8b9d1cefb0cb30cf349424f980ab
Adam Rackis <adamrackis@hotmail.com>
2026-05-23 18:08:38 -0500

    use Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   plugins: [
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
