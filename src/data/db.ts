6be196a509545ef661207bb0eec7afb3bf31c5a8
Adam Rackis <adamrackis@hotmail.com>
2026-05-27 18:03:28 -0400

    simplify drizzle code

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,9 +1,4 @@
 import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: env.HYPERDRIVE.connectionString,
-});
-
-export const db = drizzle({ client: pool });
+export const db = drizzle(env.HYPERDRIVE.connectionString);
