9e60b3f1b6de5b6e93035d5d1c7b650fe2cfdcf9
Adam Rackis <adamrackis@hotmail.com>
2026-05-29 20:38:08 -0400

    Inline global middleware context clients

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,34 @@
 import { createCsrfMiddleware, createMiddleware, createStart } from "@tanstack/react-start";
+import { betterAuth } from "better-auth";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-import { db } from "@/data/db";
-import { auth } from "@/lib/auth";
+import { getEnv } from "@/lib/cloudflareUtil";
 
 const globalContextMiddleware = createMiddleware({ type: "function" }).server(async ({ next }) => {
+  const connectionString = process.env.POSTGRES!;
+  const env = getEnv();
+
+  const pool = new Pool({
+    connectionString: env.CF ? env.HYPERDRIVE.connectionString : connectionString,
+  });
+
+  const db = drizzle({ client: pool });
+
+  const auth = betterAuth({
+    database: new Pool({
+      connectionString,
+    }),
+    plugins: [tanstackStartCookies()],
+    socialProviders: {
+      google: {
+        clientId: process.env.GOOGLE_AUTH_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET!,
+      },
+    },
+  });
+
   return next({
     context: {
       db,
