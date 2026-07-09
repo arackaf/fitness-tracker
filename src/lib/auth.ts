a515986769e95655ce0b7c80f7b3b9096e9a6a0b
Adam Rackis <arackis@spotify.com>
2026-07-08 20:51:32 -0500

    tweak baseURL for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,19 @@ import { betterAuth } from "better-auth";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { Pool } from "pg";
 import { env } from "cloudflare:workers";
+import { getRequest } from "@tanstack/react-start/server";
 
 export function createAuth(pool: Pool) {
+  let baseURL = env.BETTER_AUTH_URL;
+
+  try {
+    const req = getRequest();
+    const url = new URL(req.url);
+    baseURL = url.protocol + "//" + url.host;
+  } catch (error) {
+    baseURL = env.BETTER_AUTH_URL;
+  }
+
   return betterAuth({
     database: pool,
     plugins: [tanstackStartCookies()],
@@ -13,6 +24,7 @@ export function createAuth(pool: Pool) {
         clientSecret: env.GOOGLE_AUTH_CLIENT_SECRET!,
       },
     },
+    baseURL,
   });
 }
 
